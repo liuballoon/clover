dd82bac523136b67c1e21209e4572221810d40bf
liuballoon <liuballoon@163.com>
2021-10-19 22:03:55 +0800

    build: 启动类

---
+++ clover-api/src/main/java/com/liuballoon/CloverApplication.java
@@ -0,0 +1,17 @@
+/**
+ * @Author liuballoon
+ * @Github https://github.com/liuballoon
+ * @Create 2021/10/19 21:45
+ */
+package com.liuballoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloverApplication.class, args);
+    }
+}
