3804afce7cbf6caf4bc8c13e92f0d301f0b8da62
liuballoon <liuballoon@163.com>
2021-11-08 09:56:15 +0800

    develop: 添加属性

--- clover-pojo/src/main/java/com/liuballoon/pojo/vo/BannerItemVO.java
+++ clover-pojo/src/main/java/com/liuballoon/pojo/vo/BannerItemVO.java
@@ -14,6 +14,7 @@ import org.springframework.beans.BeanUtils;
 @Setter
 public class BannerItemVO {
     private String id;
+    private String name;
     private String coverImg;
     private Integer type;
     private String keyword;
