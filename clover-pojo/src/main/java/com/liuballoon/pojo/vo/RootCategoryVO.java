6042e8aaf606447a226fa659e4f5d3c5035e94f3
liuballoon <liuballoon@163.com>
2021-11-25 14:44:08 +0800

    build: 一级分类VO

---
+++ clover-pojo/src/main/java/com/liuballoon/pojo/vo/RootCategoryVO.java
@@ -0,0 +1,24 @@
+/**
+ * @Author liuballoon
+ * @Github https://github.com/liuballoon
+ * @Create 2021/11/21 20:37
+ */
+package com.liuballoon.pojo.vo;
+
+import com.liuballoon.pojo.model.CategoryDO;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+@Getter
+@Setter
+public class RootCategoryVO {
+    private String id;
+    private String name;
+    private String coverImg;
+    private Boolean display;
+
+    public RootCategoryVO(CategoryDO categoryDO) {
+        BeanUtils.copyProperties(categoryDO, this);
+    }
+}
