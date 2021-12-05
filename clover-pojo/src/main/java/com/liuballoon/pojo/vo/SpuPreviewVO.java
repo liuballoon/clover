115fffd39fc430bcd64f2331122b948bee5c081b
liuballoon <liuballoon@163.com>
2021-12-05 23:03:33 +0800

    build: 商品预览VO对象

---
+++ clover-pojo/src/main/java/com/liuballoon/pojo/vo/SpuPreviewVO.java
@@ -0,0 +1,20 @@
+/**
+ * @Author liuballoon
+ * @Github https://github.com/liuballoon
+ * @Create 2021/11/28 14:44
+ */
+package com.liuballoon.pojo.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SpuPreviewVO {
+    private String id;
+    private String title;
+    private String price;
+    private String discountPrice;
+    private String tags;
+    private String description;
+}
