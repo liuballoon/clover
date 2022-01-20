674b7e60b3bf3f60b3aa2b3e0cb354ea4ac5e465
liuballoon <liuballoon@163.com>
2022-01-20 21:49:35 +0800

    chore: 引入Redis

--- clover-service/src/main/java/com/liuballoon/service/impl/BannerServiceImpl.java
+++ clover-service/src/main/java/com/liuballoon/service/impl/BannerServiceImpl.java
@@ -6,6 +6,8 @@
 package com.liuballoon.service.impl;
 
 import com.liuballoon.common.exception.http.NotFoundException;
+import com.liuballoon.common.utils.RedisOperator;
+import com.liuballoon.common.utils.Serializer;
 import com.liuballoon.mapper.BannerItemMapper;
 import com.liuballoon.mapper.BannerMapper;
 import com.liuballoon.pojo.model.BannerDO;
@@ -26,8 +28,27 @@ public class BannerServiceImpl implements BannerService {
     @Autowired
     private BannerItemMapper bannerItemMapper;
 
+    @Autowired
+    private RedisOperator redisOperator;
+
     @Override
     public BannerVO getBannerByName(String name) {
+        String banner = this.redisOperator.get(name);
+        if (banner == null) {
+            BannerVO bannerVO = this.getBannerFromDB(name);
+            this.redisOperator.set(name, Serializer.objectToJson(bannerVO));
+            return bannerVO;
+        }
+        return Serializer.jsonToObject(banner, BannerVO.class);
+    }
+
+    /**
+     * 从数据库获取轮播图
+     *
+     * @param name 轮播图名称
+     * @return 轮播图
+     */
+    private BannerVO getBannerFromDB(String name) {
         BannerDO bannerDO = this.bannerMapper.selectBannerByName(name).orElseThrow(() -> new NotFoundException(10004));
         List<BannerItemVO> items = this.bannerItemMapper.selectItemsByBannerId(bannerDO.getId());
         return new BannerVO(bannerDO, items);
