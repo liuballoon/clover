fc3ff08052b3104634e7cc9dd5e64113ad13b8be
liuballoon <liuballoon@163.com>
2022-01-25 22:03:12 +0800

    chore: 完善JWT构造器

--- clover-common/src/main/java/com/liuballoon/common/auth/JWTBuilder.java
+++ clover-common/src/main/java/com/liuballoon/common/auth/JWTBuilder.java
@@ -8,29 +8,38 @@ package com.liuballoon.common.auth;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 /**
  * JWT
  */
+@Component
 public class JWTBuilder {
 
     @Value("${jwt.issuer}")
-    private static String issuer;
+    private String issuer;
 
     @Value("${jwt.secret-key}")
-    private static String secretKey;
+    private String secretKey;
 
     @Value("${jwt.expire-time}")
-    private static Long expireTime;
+    private Long expireTime;
 
-    public static String generate() {
-        Algorithm algorithm = Algorithm.HMAC256(JWTBuilder.secretKey);
+    /**
+     * 生成token
+     *
+     * @param userId 用户主键
+     * @return token
+     */
+    public String generate(String userId) {
+        Algorithm algorithm = Algorithm.HMAC256(this.secretKey);
         return JWT.create()
-                .withIssuer(issuer)
+                .withIssuer(this.issuer)
                 .withIssuedAt(new Date())
-                .withExpiresAt(new Date(expireTime))
+                .withExpiresAt(new Date(this.expireTime))
+                .withClaim("userId", userId)
                 .sign(algorithm);
     }
 }
